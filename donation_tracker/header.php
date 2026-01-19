67f46a2b2aa6763f9510680345764638785c8458
VISHALISHREE.E <vishalielu10@gmail.com>
2026-01-19 16:09:22 +0530

    Delete donation_tracker directory

--- donation_tracker/header.php
+++
@@ -1,5 +0,0 @@
-<h1 style="background-color:#1b5e20;color:white;padding:15px;border-radius:10px;width:80%;margin:20px auto;text-align:center;">
-  UDHAVIDUVOM MANITHA ARAKATTALAI
-</h1>
-<h1 style="text-align:center;">Donation Tracker</h1>
-<hr>
